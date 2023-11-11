1a3f16aed36980e4e015b44ad3d4bed90db9a140
bilal <bilal@DESKTOP-1II16LG>
2023-11-11 23:41:10 +0300

    Baslangic dosyalari gonderildi

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import ArtistView from "../views/ArtistView.vue"
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/artist",
+      component: ArtistView
+    }
+  ],
+});
+
+export default router;
